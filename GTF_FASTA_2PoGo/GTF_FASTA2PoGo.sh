72b73ad0b1ad468ad3a90de47a8284622a984eca
efwatts <141367442+efwatts@users.noreply.github.com>
2024-02-28 15:10:32 -0500

    Create GTF_FASTA2PoGo.sh

---
+++ GTF_FASTA_2PoGo/GTF_FASTA2PoGo.sh
@@ -0,0 +1,13 @@
+# It is assumed that before running this script, that the PoGo program is executable locally.
+# https://www.sanger.ac.uk/tool/pogo/
+export PATH=$PATH:/path/to/folder
+
+# It is also assumed that your GTF file and FASTA file, along with ensg_to_gene_name_map.txt are in a folder called "data"
+  # If you need to generate the ensg_to_gene_name_map.txt file, see the script extract_ensg_to_gene_map_gencode_gtf.py
+  # Update the name of your GTF and FASTA files in the script map_peptides_to_pacbio_database.py 
+
+# First, make PoGo compatible input GTF/FASTA files
+python map_peptides_to_pacbio_database.py
+
+# Then, using the newly created files, run Pogo.
+PoGo -fasta ./results/SMC_CORRECTED_orf_refined_PoGo_compatible.fasta -gtf ./results/SMC_CORRECTED_with_cds_PoGo_compatible.gtf -in ./results/CAD96_Pogo.txt -format BED
