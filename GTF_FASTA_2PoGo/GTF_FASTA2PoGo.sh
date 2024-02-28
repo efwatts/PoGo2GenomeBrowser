dcd951e125e597ba9a3ec4bb9145f77b19f12480
efwatts <141367442+efwatts@users.noreply.github.com>
2024-02-28 15:42:48 -0500

    Update GTF_FASTA2PoGo.sh

--- GTF_FASTA_2PoGo/GTF_FASTA2PoGo.sh
+++ GTF_FASTA_2PoGo/GTF_FASTA2PoGo.sh
@@ -1,8 +1,4 @@
-# It is assumed that before running this script, that the PoGo program is executable locally.
-# https://www.sanger.ac.uk/tool/pogo/
-export PATH=$PATH:/path/to/folder
-
-# It is also assumed that your GTF file and FASTA file, along with ensg_to_gene_name_map.txt are in a folder called "data"
+# It is assumed that your GTF file and FASTA file, along with ensg_to_gene_name_map.txt are in a folder called "data"
   # If you need to generate the ensg_to_gene_name_map.txt file, see the script extract_ensg_to_gene_map_gencode_gtf.py
   # Update the name of your GTF and FASTA files in the script map_peptides_to_pacbio_database.py 
 
