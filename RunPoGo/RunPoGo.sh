253133f97bc6ecbab7cffa5b6f6565d9d91aa8a8
efwatts <141367442+efwatts@users.noreply.github.com>
2024-02-28 15:42:24 -0500

    Create RunPoGo.sh

---
+++ RunPoGo/RunPoGo.sh
@@ -0,0 +1,4 @@
+# Be sure PoGo is in your PATH
+export PATH=$PATH:/path/to/file
+
+PoGo -fasta ./results/SMC_CORRECTED_orf_refined_PoGo_compatible.fasta -gtf ./results/SMC_CORRECTED_with_cds_PoGo_compatible.gtf -in ./results/CAD96_Pogo.txt -format BED
