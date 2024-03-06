64289266cdcd2e7d28d933369f126a90c67ca2f0
Emily F. Watts <watts.emily.f@gmail.com>
2024-03-06 10:21:24 -0500

    Create color2bed_isoforms.sh

---
+++ GTF2BED/color2bed_isoforms.sh
@@ -0,0 +1,5 @@
+python ./scripts/color2bed_proteinisoforms.py \
+    --bed_file_path ./data/wtc11.bed \
+    --csv_file_path ./data/as192_annotations_table_1.csv \
+    --output_bed12_path ./results/wtc11.bed12 \
+    --track_name WTC11_Protein_Isoforms
